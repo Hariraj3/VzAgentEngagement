f17542823ff84376af204f7d476056ef903ff77e
hariraj3 <hariraj@gmail.com>
2015-08-27 04:42:50 -0700

    References/Tools added

---
+++ Vz.AgentEngagement.Tests/EngagementTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vz.AgentEngagement.Engine;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Vz.AgentEngagement.Tests
+{
+    public class EngagementTest
+    {
+        
+        public void TestMethod1()
+        {
+            EngagementEngine.Initialize(@"D:\Vz.AgentEngagement\Vz.AgentEngagement\XML\");
+            EngagementResult objEngagementResult = EngagementEngine.GetEngagement(new EngagementRequest { AgentId = "Agent1" });
+            Assert.Equal(0, objEngagementResult.FreeMinutes);
+
+
+        }
+    }
+}
