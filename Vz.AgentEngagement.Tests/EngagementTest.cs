a7a21e008220cb23fbe9468cad7efe33ae5773d7
hariraj3 <hariraj@gmail.com>
2015-08-27 12:22:52 -0700

    used Nunit for testing

--- Vz.AgentEngagement.Tests/EngagementTest.cs
+++ Vz.AgentEngagement.Tests/EngagementTest.cs
@@ -4,19 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vz.AgentEngagement.Engine;
-using Xunit;
-using Xunit.Abstractions;
+//using Xunit;
+//using Xunit.Abstractions;
+using NUnit.Framework;
 
 namespace Vz.AgentEngagement.Tests
 {
+    [TestFixture]
     public class EngagementTest
     {
-        [Fact]
+        [Test]
         public void TestMethod1()
         {
             EngagementEngine.Initialize(@"D:\Vz.AgentEngagement\Vz.AgentEngagement\XML\");
             EngagementResult objEngagementResult = EngagementEngine.GetEngagement(new EngagementRequest { AgentId = "Agent1" });
-            Assert.Equal(0, objEngagementResult.FreeMinutes);
+            Assert.AreEqual(0, objEngagementResult.FreeMinutes);
 
 
         }
