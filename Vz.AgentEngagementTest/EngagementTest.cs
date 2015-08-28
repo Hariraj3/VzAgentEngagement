0d6e9c0bb453b2de62926d69caaf2070a649bdaf
Hariraj3 <hariraj@gmail.com>
2015-08-28 03:04:48 -0700

    Update EngagementTest.cs

--- Vz.AgentEngagementTest/EngagementTest.cs
+++ Vz.AgentEngagementTest/EngagementTest.cs
@@ -1,24 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vz.AgentEngagement.Engine;
-using Xunit;
-using Xunit.Abstractions;
+using NUnit.Framework;
+using System.IO;
 
-namespace Vz.AgentEngagementTest
+namespace Vz.AgentEngagement.Tests
 {
+    [TestFixture]
     public class EngagementTest
     {
+        [Test]
         public void TestMethod1()
         {
-            EngagementEngine.Initialize(@"D:\Vz.AgentEngagement\Vz.AgentEngagement\XML\");
+            //EngagementEngine.Initialize(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Content" @"D:\Vz.AgentEngagement\Vz.AgentEngagement\XML\");
+            EngagementEngine.Initialize(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "XML"));
             EngagementResult objEngagementResult = EngagementEngine.GetEngagement(new EngagementRequest { AgentId = "Agent1" });
-            Assert.Equal(0, 0);
-            //Assert.Equal(0, objEngagementResult.FreeMinutes);
-
-
+            Assert.AreEqual(0, objEngagementResult.FreeMinutes);
         }
     }
 }
